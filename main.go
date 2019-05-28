4e88eee5ee76047584748c8d640ec4f9dc65ed52
Matheus Degiovani <opensource@matheusd.com>
2019-05-28 13:25:22 -0300

    Use TLS 1.2 as minimum
    This changes the TLS config for the server to use TLS 1.2 as a minimum.
    It also explicitly only specifies newer, safe encryption algorithms.
    
    Shamelessly stolen from @davecgh :)

--- main.go
+++ main.go
@@ -132,7 +132,18 @@ func main() {
 			WriteTimeout: 30 * time.Second,
 			ReadTimeout:  30 * time.Second,
 			Addr:         ":https",
-			TLSConfig:    &tls.Config{GetCertificate: m.GetCertificate},
+			TLSConfig: &tls.Config{
+				GetCertificate: m.GetCertificate,
+				MinVersion:     tls.VersionTLS12,
+				CipherSuites: []uint16{
+					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+					tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+				},
+			},
 		}
 		if err := httpServer.ListenAndServeTLS("", ""); err != nil {
 			log.Critical(err)
@@ -143,4 +154,10 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	<-c
+}
+
+func init() {
+	// Support TLS 1.3.
+	os.Setenv("GODEBUG", os.Getenv("GODEBUG")+",tls13=1")
+
 }
